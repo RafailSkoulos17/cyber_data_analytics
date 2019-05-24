b907d38bf3d54159a24e21cbde793241a7bca620
LENOVO-PC\Vasilis <el12021@central.ntua.gr>
2019-05-24 22:38:32 +0200

    Plots and naive prediction added

--- anomaly_detection/familiarization.py
+++ anomaly_detection/familiarization.py
@@ -3,8 +3,6 @@ from sklearn.preprocessing import StandardScaler
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-# from dtaidistance import dtw
-# from dtaidistance import dtw_visualisation as dtwvis
 import numpy as np
 
 
@@ -37,7 +35,7 @@ def make_lineplot(df, features, dataset, start_date, end_date):
     """
     # axes = df.loc[start_date:end_date][features].plot(figsize=(11, 9), subplots=True)
     fig, ax = plt.subplots(len(features), 1, sharex=True)
-    colors = ['b', 'r', 'g']
+    colors = ['b', 'r', 'g', 'c', 'm', 'y', 'k']
     for ind, feature in enumerate(features):
         ax[ind].plot(df.loc[start_date:end_date][feature], colors[ind])
         ax[ind].xaxis.set_major_locator(mdates.DayLocator([5, 10, 15, 20, 25, 30]))
@@ -51,6 +49,32 @@ def make_lineplot(df, features, dataset, start_date, end_date):
     else:
         plt.savefig('plots/%s/lineplot_%s.png' % (dataset, features[0]), bbox_inches='tight')
 
+
+def plot_against(test_df, pred_df, features, dataset):
+    # axes = df.loc[start_date:end_date][features].plot(figsize=(11, 9), subplots=True)
+    fig, ax = plt.subplots(len(features), 1, sharex=True)
+    colors = ['b', 'r', 'g', 'c', 'm', 'y', 'k']
+    for ind, feature in enumerate(features):
+        ax[ind].plot(test_df[feature], colors[2*ind], label='true')
+        ax[ind].plot(pred_df[feature], colors[2*ind+1], label='predicted')
+        ax[ind].xaxis.set_major_locator(mdates.DayLocator([5, 10, 15, 20, 25, 30]))
+        ax[ind].xaxis.set_major_formatter(mdates.DateFormatter('%d/%m'))
+        ax[ind].set_ylabel(feature)
+        ax[ind].grid()
+        ax[ind].legend()
+    plt.xticks(rotation=45)
+    plt.xlabel("time")
+    if len(features) > 1:
+        plt.savefig('plots/%s/lineplot_%s.png' % (dataset, '_'.join(features)), bbox_inches='tight')
+    else:
+        plt.savefig('plots/%s/lineplot_%s.png' % (dataset, features[0]), bbox_inches='tight')
+
+
+def sliding_window_predict(df, k, w='boxcar'):
+    test_df = df.loc['2017-01-04 00':'2017-04-01 00']
+    shifted = df.rolling(k, win_type=w).mean().shift(1)
+    plot_against(test_df, shifted.loc['2017-01-04 00':'2017-04-01 00'], ['L_T1', 'L_T2', 'L_T3'], 'naive_predictions')
+
 # Dataset columns -> date of the event (hours in a day) -> DATETIME
 # tank levels -> L_T1, L_T2, L_T3, L_T4, L_T5, L_T6, L_T7
 # status and flow of pumps and valves -> F_PU1, S_PU1, F_PU2, S_PU2, F_PU3, S_PU3, F_PU4, S_PU4, F_PU5, S_PU5, F_PU6,
@@ -59,33 +83,45 @@ def make_lineplot(df, features, dataset, start_date, end_date):
 # P_J422
 # labels -> ATT_FLAG (normal: 0, under attack: 1, unlabeled: -999)
 
-# training dataset 1 with no attacks from 06/01/14 00 to 06/01/15 00
-# train_df1 = pd.read_csv('BATADAL_datasets/BATADAL_training_dataset1.csv', parse_dates=[0],
-#                         date_parser=lambda x: pd.to_datetime(x, format="%d/%m/%y %H"))
-# scaled_df1, train_y1 = scale_and_separate(train_df1)
 
+# training dataset 1 with no attacks from 06/01/14 00 to 06/01/15 00
+print('Reading the dataset')
+train_df1 = pd.read_csv('BATADAL_datasets/BATADAL_training_dataset1.csv', index_col=0, parse_dates=[0],
+                        date_parser=lambda x: pd.to_datetime(x, format="%d/%m/%y %H"))
+print('Applying scaling on the data')
+scaled_df1, train_y1 = scale_and_separate(train_df1)
+# print('Plotting')
+# make_lineplot(scaled_df1, ['L_T7', 'F_PU10', 'F_PU11'], 'training1', '2014-09-01', '2014-09-30')
+# make_lineplot(scaled_df1, ['L_T1', 'F_PU1', 'F_PU2'], 'training1', '2014-10-01', '2014-11-10')
+# make_lineplot(train_df1, ['L_T1', 'S_PU1', 'S_PU2'], 'training1', '2014-10-01', '2014-11-10')
+# make_lineplot(scaled_df1, ['L_T4', 'F_PU7'], 'training1', '2014-11-25', '2014-12-20')
 
 # training dataset 2 with attack from 04/07/16 00 to 25/12/16 00
 print('Reading the dataset')
 train_df2 = pd.read_csv('BATADAL_datasets/BATADAL_training_dataset2.csv', index_col=0, parse_dates=[0],
                         date_parser=lambda x: pd.to_datetime(x, format="%d/%m/%y %H"))
-# train_df2.columns = train_df2.columns.str.lstrip()
+# # train_df2.columns = train_df2.columns.str.lstrip()
 print('Applying scaling on the data')
 scaled_df2, train_y2 = scale_and_separate(train_df2)
-print('Plotting')
-make_lineplot(scaled_df2, ['L_T7', 'F_PU10', 'F_PU11'], 'training2', '2016-09-01', '2016-09-30')
-make_lineplot(scaled_df2, ['L_T1', 'F_PU1', 'F_PU2'], 'training2', '2016-10-01', '2016-11-10')
-make_lineplot(train_df2, ['L_T1', 'S_PU1', 'S_PU2'], 'training2', '2016-10-01', '2016-11-10')
-make_lineplot(scaled_df2, ['L_T4', 'F_PU7'], 'training2', '2016-11-25', '2016-12-20')
+# print('Plotting')
+# make_lineplot(scaled_df2, ['L_T7', 'F_PU10', 'F_PU11'], 'training2', '2016-09-01', '2016-09-30')
+# make_lineplot(scaled_df2, ['L_T1', 'F_PU1', 'F_PU2'], 'training2', '2016-10-01', '2016-11-10')
+# make_lineplot(train_df2, ['L_T1', 'S_PU1', 'S_PU2'], 'training2', '2016-10-01', '2016-11-10')
+# make_lineplot(scaled_df2, ['L_T4', 'F_PU7'], 'training2', '2016-11-25', '2016-12-20')
 
-# test dataset with attacks from 04/01/17 00 to 01/04/17 00
+# # test dataset with attacks from 04/01/17 00 to 01/04/17 00
 print('Reading the dataset')
 test_df = pd.read_csv('BATADAL_datasets/BATADAL_test_dataset.csv', index_col=0, parse_dates=[0],
                       date_parser=lambda x: pd.to_datetime(x, format="%d/%m/%y %H"))
 print('Applying scaling on the data')
 scaled_test_df, _ = scale_and_separate(test_df, labels=False)
-print('Plotting')
-make_lineplot(scaled_test_df, ['L_T7', 'F_PU10', 'F_PU11'], 'test', '2016-09-01', '2016-09-30')
-make_lineplot(scaled_test_df, ['L_T1', 'F_PU1', 'F_PU2'], 'test', '2016-10-01', '2016-11-10')
-make_lineplot(train_df2, ['L_T1', 'S_PU1', 'S_PU2'], 'test', '2016-10-01', '2016-11-10')
-make_lineplot(scaled_test_df, ['L_T4', 'F_PU7'], 'test', '2016-11-25', '2016-12-20')
+# print('Plotting')
+# make_lineplot(scaled_test_df, ['L_T3', 'F_PU4', 'F_PU5'], 'test', '2017-01-04', '2017-02-05')
+# make_lineplot(test_df, ['F_PU3', 'S_PU3'], 'test', '2017-02-01', '2017-02-28')
+# make_lineplot(test_df, ['L_T2', 'F_V2', 'S_V2'], 'test', '2017-01-25', '2017-03-05')
+# make_lineplot(scaled_test_df, ['L_T2', 'P_J14', 'P_J422'], 'test', '2017-01-25', '2017-03-05')
+# make_lineplot(scaled_test_df, ['L_T7', 'F_PU10', 'F_PU11'], 'test', '2017-03-01', '2017-03-30')
+# make_lineplot(scaled_test_df, ['L_T7', 'P_J14', 'P_J422'], 'test', '2017-03-01', '2017-03-30')
+# make_lineplot(scaled_test_df, ['L_T4', 'L_T6'], 'test', '2017-03-01', '2017-03-30')
+
+sliding_window_predict(pd.concat([scaled_df1, scaled_df2, scaled_test_df], ignore_index=False), 24, w='triang')
