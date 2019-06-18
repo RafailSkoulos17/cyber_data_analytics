e3067f577b899484fa233cdd90c0fe42c34a341e
LENOVO-PC\Vasilis <el12021@central.ntua.gr>
2019-06-18 10:47:20 +0200

    Better adversarial implementation

--- streams/adversarial.py
+++ streams/adversarial.py
@@ -27,56 +27,33 @@ infected_ips = ['147.32.84.165', '147.32.84.191', '147.32.84.192', '147.32.84.19
                 '147.32.84.205', '147.32.84.206', '147.32.84.207', '147.32.84.208', '147.32.84.209']
 
 
-def insert_row(row_number, df, row_value):
-    start_upper = 0
-    end_upper = row_number
-    start_lower = row_number
-    end_lower = df.shape[0]
-
-    # Create a list of upper_half index
-    upper_half = [*range(start_upper, end_upper, 1)]
-
-    # Create a list of lower_half index
-    lower_half = [*range(start_lower, end_lower, 1)]
-
-    # Increment the value of lower half by 1
-    lower_half = [x.__add__(1) for x in lower_half]
-
-    # Combine the two lists
-    index_ = upper_half + lower_half
-
-    # Update the index of the dataframe
-    df.index = index_
-
-    # Insert a row at the end and sort the index labels
-    df.loc[row_number] = row_value
-    df = df.sort_index()
-
-    return df
-
-
 def make_adversarial(df, altered):
     new_df = df.copy()
-    new_df.sort_values('date', ascending=True, inplace=True)
-    # first specify the number of adversarial flows to be constructed
-    n = int(0.25*len(df[df['label'] == 'Botnet']))
+    # first specify the number of adversarial flows to be constructed - currently 1% of the botnet data
+    n = int(0.01*len(df[df['label'] == 'Botnet']))
     for i in range(n):
         ind = randint(0, new_df.shape[0]-2)
+
+        # create a new date that does not already exist in the dataset
+        new_date = new_df.index[ind] + random()*(new_df.index[ind+1]-new_df.index[ind])
+
+        # create new flow
         new_flow = []
-        new_flow += [new_df['date'][ind] + random()*(new_df['date'][ind+1]-new_df['date'][ind])]
-        new_flow += [new_df['duration'][ind]]
-        new_flow += [new_df['protocol'][ind]]
-        new_flow += [infected_ips[randint(0, 9)]]
-        new_flow += [new_df['src_port'][ind]]
-        new_flow += [new_df['dst_ip'][ind]]
-        new_flow += [new_df['dst_port'][ind]]
-        new_flow += [new_df['flags'][ind]]
-        new_flow += [new_df['tos'][ind]]
-        new_flow += [new_df['packets'][ind] + 0 if 'packets' not in altered.keys() else altered['packets']]
-        new_flow += [new_df['bytes'][ind] + 0 if 'bytes' not in altered.keys() else altered['bytes']]
-        new_flow += [new_df['flows'][ind]]
-        new_flow += ['Artificial']
-        new_df = insert_row(ind+1, new_df, new_flow)
+        new_flow += [new_df['duration'][ind]]  # duration
+        new_flow += [new_df['protocol'][ind]]  # and protocol remain the same
+        new_flow += [infected_ips[randint(0, 9)]]  # choose one of the botnet ips
+        new_flow += [new_df['src_port'][ind]]  # source port
+        new_flow += [new_df['dst_ip'][ind]]  # destination ip
+        new_flow += [new_df['dst_port'][ind]]  # destination port
+        new_flow += [new_df['flags'][ind]]  # flags
+        new_flow += [new_df['tos'][ind]]  # and tos remain the same
+        new_flow += [new_df['packets'][ind] + 0 if 'packets' not in altered.keys() else altered['packets']]  # alter packets
+        new_flow += [new_df['bytes'][ind] + 0 if 'bytes' not in altered.keys() else altered['bytes']]  # alter bytes
+        new_flow += [new_df['flows'][ind]]  # flow remains the same
+        new_flow += ['Artificial']  # add the 'Artificial' label just for later plotting issues
+
+        # TODO: check how to optimize it for memory
+        new_df.loc[new_date] = new_flow  # add the new flow to the dataset
     return new_df
 
 
@@ -91,11 +68,18 @@ if __name__ == '__main__':
     data['packets'] = data['packets'].astype(int)
     data['bytes'] = data['bytes'].astype(int)
 
+    data.sort_values('date', ascending=True, inplace=True)
+
+    # set date as index in the dataframe
+    data = data.set_index(data.date)
+    data.drop('date', axis=1, inplace=True)
+
+    # create all the adversarial datasets
     for types in perturbation_types.keys():
         for step in perturbation_steps.keys():
+            print('creating type %d with step %d' % (types, step))
             to_be_altered = {}
             for type in perturbation_types[types]:
                 to_be_altered[type] = perturbation_steps[step][type]
             adv_df = make_adversarial(data, to_be_altered)
             adv_df.to_pickle('altered_%s_step_%d.pkl' % ('_'.join(perturbation_types[types]), step))
-
